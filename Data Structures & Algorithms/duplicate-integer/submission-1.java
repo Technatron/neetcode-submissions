d566177b05e9da17fd84220058dabb7e79605e72
Merlin <99322785+Technatron@users.noreply.github.com>
2026-09-24 02:02:17 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+
+        HashMap<Integer, Integer> map = new HashMap();
+        for(int i=0; i<nums.length; i++){
+            if(map.get(nums[i]) == null ){
+                map.put(nums[i], 1);
+            }
+            else{
+                map.put(nums[i], map.get(nums[i])+1);
+            }
+
+            if(map.get(nums[i])>1) return true;
+        }
+
+        // System.out.println(map);
+
+        return false;
+    }
+}
